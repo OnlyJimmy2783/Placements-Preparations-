cfd252d003a84ff2237f22dec7bed5e5acccef37
Jimit Patel <jimitpatel2783@gmail.com>
2025-08-17 17:07:40 +0530

    LCM & GCD

---
+++ lcm_gcd_finder.py
@@ -0,0 +1,12 @@
+#importing math module for LCM and GCD functions
+import math
+
+a = 12
+b = 18
+
+#pre-built functions to find LCM and GCD
+Result_lcm = math.lcm(a, b)
+Result_gcd = math.gcd(a, b)
+
+print("Least Commmen Multiple:-",Result_lcm)
+print("Greatest Common Divisor:-",Result_gcd)
